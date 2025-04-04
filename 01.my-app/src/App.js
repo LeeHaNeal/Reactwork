147951daffbe5ce3304d82e924710311528c9177
LeeHaNeal <harull817@naver.com>
2025-04-04 18:17:52 +0900

    2025년 4월 4일

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>React Test Page</h1>
+      <p>오늘부터 리액트 시작</p>
+      <h2>종료시작이 다가옵니다.</h2>
+    </div> 
+  );
+}
+
+export default App;
