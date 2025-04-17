bf82ee7dc9aa0e4b2c33c0c9cd5878a9191dbd03
LeeHaNeal <harull817@naver.com>
2025-04-17 15:15:17 +0900

    2025년 04월 17일

---
+++ shopping-mall/src/data/ProductList.js
@@ -0,0 +1,42 @@
+// const num1 = 10;
+// const num2 = 20;
+
+// export default {num1,num2};
+
+const pList = [
+    {
+      id : 1 ,
+      title : 'vest',
+      content : '마리 링거 반팔 티셔츠 (MAFBTS07MWH)',
+      price : 25000
+    },
+    {
+      id : 2 ,
+      title : 'outer',
+      content : '트렌드 빈티지 벌룬 투라인 워싱 오버핏 데님 청 트러커 자켓',
+      price : 45000
+    },
+    {
+      id : 3 ,
+      title : 'sportswear',
+      content : '벨로아 트레이닝 셋업 벨벳 벨로아 트레이닝 트랙 집업',
+      price : 55000
+    },
+    
+  ]
+
+
+  export default pList;
+
+
+
+
+
+
+
+
+
+
+
+
+
